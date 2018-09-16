afc4be7b7cff9eb1e3bb8663e393da1440a4afe5
rubyliuqq <1076795115@qq.com>
2018-09-16 21:34:14 +0800

    路由的设定

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+// 增加个人收获地址页面
 import AddSite from '../page/member/children/addSite.vue';
+// 首页
 import Home from '../page/home/home.vue';
+// 个人中心
 import Member from '../page/member/member.vue';
+// 个人中心信息页面，换头像，改密码等
 import MemberInfo from '../page/member/children/memberInfo.vue';
+// 个人收获地址页面
 import MySite from '../page/member/children/mySite.vue';
+// 订单列表页
 import Order from '../page/order/order.vue';
+// 登陆页
 import Login from '../page/login/login.vue';
+// 商品搜索页
 import Search from '../page/search/search.vue';
+// 地址定位页面，搜索收获地址
+import Site from '../page/site/site.vue';
+// 城市选择页
+import CitySiteSelect from '../page/site/citySiteSelect.vue';
 
 Vue.use(Router);
 
@@ -45,6 +57,16 @@ export default new Router({
       path: '/search',
       name: 'search',
       component: Search,
+    },
+    {
+      path: '/site',
+      name: 'site',
+      component: Site,
+    },
+    {
+      path: '/citySiteSelect',
+      name: 'citySiteSelect',
+      component: CitySiteSelect,
     },
     {
       path: '/order',
